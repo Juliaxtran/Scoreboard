68f00c3cca43cea72e17c35eab2fb0878b59f57a
Julia Tran <julia.kim.tran@hotmail.com>
2023-02-22 14:04:55 -0500

    Connect to database

--- server/app.js
+++ server/app.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
 
 const app = express();
+const port = process.env.PORT || 4000;
+dotenv.config();
+
 app.use(bodyParser.json());
 
-mongoose.set("strictQuery", false);
-mongoose.connect('mongodb://localhost:27017/scoreboard', { useNewUrlParser: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+// Connect to database
+mongoose.set('strictQuery', true)
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, { useNewUrlParser: true })
+    console.log('Connected to database')
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+
+
 
 //Use Routes
 // app.use('/api/items', items);
 
-const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+
+app.listen(port,
+  () => {
+    connect();
+    console.log(`Server started on port ${port}`)
+  }
+  );
