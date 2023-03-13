e35301093a0514572b67b71cd07795640c156f6f
Julia Tran <julia.kim.tran@hotmail.com>
2023-03-13 17:21:41 -0400

    Psql setup

--- server/app.js
+++ server/app.js
@@ -7,6 +7,26 @@ const matchRoutes = require('./routes/match');
 const gameRoutes = require('./routes/game');
 const dotenv = require('dotenv');
 const session = require('express-session');
+const { Pool } = require('pg');
+
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env;
+
+const pool = new Pool({
+	user: DB_USER,
+	host: DB_HOST,
+	password: DB_PASSWORD,
+	port: DB_PORT,
+	database: DB_DATABASE,
+})
+
+pool.connect().then(() => {
+	console.log("Database connection established.")
+}).catch( e => {
+	throw new Error(e);
+})
+
+
+
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,15 +35,15 @@ dotenv.config();
 app.use(bodyParser.json());
 
 // Connect to database
-mongoose.set('strictQuery', true)
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO, { useNewUrlParser: true })
-    console.log('Connected to database')
-  } catch (err) {
-    console.log(err)
-  }
-}
+// mongoose.set('strictQuery', true)
+// const connect = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO, { useNewUrlParser: true })
+//     console.log('Connected to database')
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
 
 //CORS for all routes
 
@@ -49,7 +69,7 @@ app.get('/health', (req, res) => {
 
 app.listen(port,
   () => {
-    connect();
+    // connect();
     console.log(`Server started on port ${port}`)
   }
 );
