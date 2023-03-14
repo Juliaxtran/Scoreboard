303fd02cb0ba8543cc588e2e54d57fc8820f09a1
Patrice Pavico <andrea_pavico98@yahoo.ca>
2023-03-14 19:37:31 -0400

    adds match to group

--- server/routes/match.js
+++ server/routes/match.js
@@ -1,31 +1,62 @@
 const router = require("express").Router();
 
 module.exports = (db, dbQueries) => {
-  router.post("/", (req, res) => {
-    const { game_id, date } = req.body;
-    let match_id;
+router.post("/:groupId", async (req, res) => {
+const { game_id, date } = req.body;
+const { groupId } = req.params;
+let match_id;
 
-    dbQueries.createMatch(game_id, date, db)
-    .then((match) => {
-      if (match) {
-        match_id = match.id;
-        console.log(match_id);
+try {
+  const match = await dbQueries.createMatch(game_id, date, db);
+  if (match) {
+    match_id = match.id;
+    console.log(match_id);
 
-        res.status(200).send({
-          success: true,
-          message: "Match created",
-          match: match,
-        });
-      } else {
-        res.status(400).send({
-          success: false,
-          message: "Match not created",
-          match: match,
-        });
-      }
-    })
+    res.status(200).send({
+      success: true,
+      message: "Match created",
+      match: match,
+    });
+  } else {
+    res.status(400).send({
+      success: false,
+      message: "Match not created",
+      match: match,
+    });
+  }
+  //Part 2 of creating a match
+const matchToGroup = await dbQueries.addGroupMatch(match_id, groupId, db);
+
+if(matchToGroup){
+    res.status(200).send({
+        success: true,
+        message: "Match added to group",
+        match: match,
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} catch (error) {
+  console.error(error);
+  res.status(500).send({
+    success: false,
+    message: "Server Error",
   });
+}
+});
 
-
-  return router;
+return router;
 };
