5db7ad875ad240df69c6bc98a391bf95a67b6425
Julia Tran <julia.kim.tran@hotmail.com>
2023-03-15 12:58:02 -0400

    Add login functionality

--- client/scoreboard/src/components/LoginForm.js
+++ client/scoreboard/src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react'
+import React, { useState, useContext } from 'react'
 import {
   useMediaQuery,
   Box,
@@ -32,8 +32,9 @@ export default function LoginForm() {
     axios.post("http://localhost:4000/player/login", formData).then((res) => {
       console.log(res.data.player);
       const success = res.status === 200;
-      if(success) {
-        setError("Login Successful")
+      if (success) {
+        setError("Login Successful");
+        setTimeout(() => setError(null), 3000);
       } else {
         setError("Login Failed")
       }
@@ -48,60 +49,58 @@ export default function LoginForm() {
   const isMobile = useMediaQuery("(max-width:420px)");
   return (
     <form className="authentication" onSubmit={handleSubmit}>
-    <Box
-      sx={{
-        "& > :not(style)": {
-          width: isMobile ? 336 : 400,
-          height:  350,
-          position: "absolute",
-          top: 200,
-          marginLeft: isMobile ? 2 : 60,
-          paddingLeft: 2,
-          paddingRight: 2,
-        },
-      }}
-    >
-      <Paper elevation={3}>
-        <Typography
-          variant="h5"
-          component="div"
-          sx={{
-            fontFamily: "JetBrains Mono, monospace",
-            textAlign: "center",
-            fontWeight: "bold",
-            marginTop: 5,
-          }}
-        >
-          Login
-        </Typography>
-        <div className="authentication-form">
-          <TextField required
-          label="Email"
-          name='email'
-          value={formData.email}
-          onChange={handleChange}
-          autoComplete='username'/>
-          <TextField
-            id="outlined-password-input"
-            label="Password*"
-            type="password"
-            name='password'
-            value={formData.password}
-            onChange={handleChange}
-            autoComplete="current-password"
-          />
-
-          <Button variant="contained" color="error" type="submit">
+      <Box
+        sx={{
+          "& > :not(style)": {
+            width: isMobile ? 336 : 400,
+            height: 350,
+            marginLeft: isMobile ? 2 : 60,
+            paddingLeft: 2,
+            paddingRight: 2,
+          },
+        }}
+      >
+        <Paper elevation={3}>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              fontFamily: "JetBrains Mono, monospace",
+              textAlign: "center",
+              fontWeight: "bold",
+              marginTop: 5,
+            }}
+          >
             Login
-          </Button>
-        </div>
-      </Paper>
-    </Box>
-    {error && (
-  <Alert severity={error === "Login Successful" ? "success" : "error"}>
-    {error}
-  </Alert>
-)}
-  </form>
+          </Typography>
+          <div className="authentication-form">
+            <TextField required
+              label="Email"
+              name='email'
+              value={formData.email}
+              onChange={handleChange}
+              autoComplete='username' />
+            <TextField
+              id="outlined-password-input"
+              label="Password*"
+              type="password"
+              name='password'
+              value={formData.password}
+              onChange={handleChange}
+              autoComplete="current-password"
+            />
+
+            <Button variant="contained" color="error" type="submit">
+              Login
+            </Button>
+          </div>
+        </Paper>
+      </Box>
+      {error && (
+        <Alert severity={error === "Login Successful" ? "success" : "error"}>
+          {error}
+        </Alert>
+      )}
+    </form>
   )
 }
--- client/scoreboard/src/pages/LoginPage.js
+++ client/scoreboard/src/pages/LoginPage.js
@@ -1,17 +1,22 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import LoginForm from "../components/LoginForm";
+import "./LoginPage.css";
 
 
 const LoginPage = () => {
 
 
   return (
-    <>
+    <div className="LoginPage">
       <NavBar />
+      <div className='login'>
+      <div className='loginCopy'>
+      Scorebook app that allows you to keep track of scores and statistics for your favorite games. Whether you're playing with friends, family, or in a league, ScoreBoard makes it easy to record and track scores, as well as individual and team statistics. Scorebook for the competitive friends.
+      </div>
       <LoginForm />
-
-    </>
+      </div>
+      </div>
   );
 };
 
