e7c4d6da5cb4fc501577af1d17dcfb876fcd8147
Julia Tran <julia.kim.tran@hotmail.com>
2023-03-22 15:23:55 -0400

    Add images

--- client/scoreboard/src/components/LeaderBoard.js
+++ client/scoreboard/src/components/LeaderBoard.js
@@ -4,6 +4,10 @@ import { Box } from "@mui/system";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { Context } from "../context/StateContext";
+import trophy1 from '../assets/trophy1.png';
+import trophy2 from '../assets/trophy2.png';
+import trophy3 from '../assets/trophy3.png';
+import trophy4 from '../assets/trophy4.png';
 
 const LeaderBoard = () => {
   const isMobile = useMediaQuery("(max-width:450px)");
@@ -37,14 +41,14 @@ const LeaderBoard = () => {
         }}
       >
         <Paper sx={{ backgroundColor: "#cdcdcdf5" }} elevation={0}>
-          <Link to="/players" style={{ textDecoration: "none", color:'black' }}>
+          <Link to="/players" style={{ textDecoration: "none", color: 'black' }}>
             <h1>Leaderboard</h1>
           </Link>
           <Box
             sx={{
               display: "flex",
               flexWrap: "wrap",
-              textAlign: "center",
+              textAlign: "left",
               "& > :not(style)": {
                 m: 1,
                 width: 390,
@@ -54,29 +58,42 @@ const LeaderBoard = () => {
           >
 
             {/* Profile Container */}
-            {Array.isArray(players) && players.map(player => {
+            {Array.isArray(players) && players.map((player, index) => {
+                let trophyImage;
+                if (index === 0) {
+                  trophyImage = trophy1;
+                } else if (index === 1) {
+                  trophyImage = trophy2 ;
+                } else if (index === 2) {
+                  trophyImage = trophy3;
+                } else {
+                  trophyImage = trophy4;
+                }
+
+
               return (
                 <Paper key={player.id}>
-                <div className="leader-box">
-                  {/* Avatar icon */}
-                  <Avatar
-                    src="/broken-image.jpg"
-                    sx={{
-                      width: 120,
-                      height: 120,
-                      color: "coral",
-                      backgroundColor: "lightblue",
-                    }}
-                  />
-                  {/* Profile Info */}
-                  <div className="leader-info">
-                    <h1>{player.name}</h1>
-                    <h3>Wins: {player.total_wins}</h3>
-                    <h3>Total matches : {player.total_matches}</h3>
-                    <h3>Win Rate: % {Number(player.win_ratio).toFixed(1)}</h3>
+                  <div className="leader-box">
+                    <h1>{index + 1}</h1>
+                    {/* Avatar icon */}
+                    <Avatar
+                      src={trophyImage}
+                      sx={{
+                        width: 100,
+                        height: 100,
+                        backgroundColor: "#cdcdcdf5",
+                        padding: 1, 
+                      }}
+                    />
+                    {/* Profile Info */}
+                    <div className="leader-info">
+                      <h1>{player.name}</h1>
+                      <h3>Wins: {player.total_wins}</h3>
+                      <h3>Total matches : {player.total_matches}</h3>
+                      <h3>Win Rate: % {Number(player.win_ratio).toFixed(1)}</h3>
+                    </div>
                   </div>
-                </div>
-              </Paper>
+                </Paper>
               )
             })}
           </Box>
