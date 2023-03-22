83d166056629aafe9e376cc1cf0869c499a3697a
Patrice Pavico <andrea_pavico98@yahoo.ca>
2023-03-21 23:13:04 -0400

    adds queries for ranking and matches

--- server/db/queries/01_allMatchInfo.sql
+++ server/db/queries/01_allMatchInfo.sql
@@ -23,3 +23,30 @@ JOIN Matches_Players mp ON m.id = mp.match_id
 JOIN Players p ON mp.player_id = p.id
 GROUP BY m.id, g.name, played_on, g2.id;
 
+------ count number of losses and wins for each player in a group and win percentage
+
+SELECT p.id, p.name, COUNT(CASE WHEN mp.is_winner THEN 1 END) AS wins,   (SELECT COUNT(*) FROM Matches_Players mp2 WHERE mp2.player_id = p.id) - COUNT(CASE WHEN mp.is_winner THEN 1 END) AS losses
+FROM Matches m
+JOIN Games g ON m.game_id = g.id
+JOIN Groups g2 ON g2.id = g.group_id AND g2.id = 2
+JOIN Matches_Players mp ON m.id = mp.match_id
+JOIN Players p ON mp.player_id = p.id
+GROUP BY p.id, p.name;
+
+
+SELECT p.id, p.name, 
+  COUNT(CASE WHEN mp.is_winner THEN 1 END) AS wins, 
+  (SELECT COUNT(*) FROM Matches_Players mp2
+    JOIN Matches m ON m.id = mp2.match_id
+    JOIN Games g ON g.id = m.game_id
+    WHERE mp2.player_id = p.id AND g.group_id = 2
+  ) - COUNT(CASE WHEN mp.is_winner THEN 1 END) AS losses, 
+    (COUNT(CASE WHEN mp.is_winner THEN 1 END) / count(*) *100) AS win_rate
+FROM Matches_Players mp
+JOIN Players p ON mp.player_id = p.id
+JOIN Matches m ON m.id = mp.match_id
+JOIN Games g ON g.id = m.game_id
+WHERE g.group_id = 2
+GROUP BY p.id, p.name;
+
+
---
+++ server/db/queries/02_Game_Queries.sql
@@ -0,0 +1,51 @@
+---- most max wins and max losses in a group for a specific game --- 
+
+--- most losses in a group for a specific game ---
+select games.name,
+ players.name, 
+ count(CASE WHEN matches_players.is_winner THEN 1 END) as wins,   
+    (select count(*)) - count(CASE WHEN matches_players.is_winner THEN 1 END) as losses
+from games
+join groups on games.group_id = groups.id
+join matches on games.id = matches.game_id
+join matches_players on matches.id = matches_players.match_id
+join players on matches_players.player_id = players.id
+where groups.id = 1
+and games.name = 'Catan'
+group by games.name, players.name
+order by losses desc
+limit 1
+; 
+
+
+-- most wins in a group for a specific game ---
+select games.name,
+ players.name, 
+ count(CASE WHEN matches_players.is_winner THEN 1 END) as wins,   
+    (select count(*)) - count(CASE WHEN matches_players.is_winner THEN 1 END) as losses
+from games
+join groups on games.group_id = groups.id
+join matches on games.id = matches.game_id
+join matches_players on matches.id = matches_players.match_id
+join players on matches_players.player_id = players.id
+where groups.id = 2
+and games.id = 4
+group by games.name, players.name
+order by losses, played_on desc
+limit 1
+; 
+
+
+-- select games.name,
+--  players.name, 
+--  count(matches_players.is_winner) as wins,   
+--     (select count(*)) - count(matches_players.is_winner) as losses
+-- from games 
+-- join groups on games.group_id = groups.id
+-- join matches on games.id = matches.game_id
+-- join matches_players on matches.id = matches_players.match_id
+-- join players on matches_players.player_id = players.id
+-- where groups.id = 1
+-- and games.name = 'Catan'
+-- group by games.name, players.name
+-- ; 
