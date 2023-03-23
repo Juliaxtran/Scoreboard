66235f0127fb2cf44ec963220611e6fd2ee22a37
Julia Tran <julia.kim.tran@hotmail.com>
2023-03-23 18:54:52 -0400

    Take out params

--- client/scoreboard/src/pages/Dashboard.js
+++ client/scoreboard/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+
 import AddButton from "../components/AddButton";
 import GameBoard from "../components/GameBoard";
 import LeaderBoard from "../components/LeaderBoard";
@@ -10,16 +10,12 @@ import "./Dashboard.css";
 
 const Dashboard = () => {
 
-const {group_id} = useParams();
-
-
 
 
   return (
     <>
       <NavBar />
       <AddButton className='addButtons'/>
-      <p>GroupId:{group_id}</p>
       <div className="leaderboard-game-match" style={{ marginLeft: 20}}>
         <LeaderBoard />
         <div className="game-match">
