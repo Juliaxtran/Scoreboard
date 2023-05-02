a9cb252238ad1faed170329cb57c734f3dc0d409
Patrice Pavico <andrea_pavico98@yahoo.ca>
2023-05-01 21:35:02 -0400

    adds config for render

--- server/configs/db.config.js
+++ server/configs/db.config.js
@@ -3,13 +3,32 @@ const { Pool } = require('pg');
 
 const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env;
 
-const pool = new Pool({
-	user: DB_USER,
-	host: DB_HOST,
-	password: DB_PASSWORD,
-	port: DB_PORT,
-	database: DB_DATABASE,
-})
+
+const isLocal = process.env.DB_HOST === 'localhost';
+
+let config; 
+if (isLocal) {
+	require('dotenv').config();
+	config = {
+	  user: DB_USER,
+	  password:DB_PASSWORD,
+	  host: DB_HOST,
+	  port: DB_PORT,
+	  database: DB_DATABASE
+	};
+  } else {
+	// Use the configuration for Render or other hosting services
+	config = {
+	  user: process.env.your_db_user,
+	  password: process.env.your_db_password,
+	  host: process.env.your_db_host,
+	  port: process.env.your_db_port,
+	  database: process.env.your_db_name
+	};
+  }
+  
+  const pool = new Pool(config);
+
 
 pool.connect().then(() => {
 	console.log("Database connection established.")
